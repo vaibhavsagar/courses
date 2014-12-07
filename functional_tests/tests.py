befae6070e94da631e28c2f58bc1cb686320e73a
vaibhavsagar <vaibhavsagar@gmail.com>
2014-12-07 14:58:28 +0700

    Fix test failure.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -105,7 +105,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # Edith goes to the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # She notices the input box is nicely centered
