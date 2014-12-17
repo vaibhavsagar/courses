70574d12e5a35ed5d16fe6375ef201c4eeeca9cb
vaibhavsagar <vaibhavsagar@gmail.com>
2014-12-17 22:51:02 +0700

    Custom Persona auth backend + custom user model.

--- superlists/settings.py
+++ superlists/settings.py
@@ -88,6 +88,11 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'superlists', 'static'),
 )
 
+AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = (
+    'accounts.authentication.PersonaAuthenticationBackend',
+)
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
