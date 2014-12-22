098367c5d56a21a2d79586129f6b427a9be6b706
vaibhavsagar <vaibhavsagar@gmail.com>
2014-12-23 00:48:29 +0700

    Add list owners via forms. More isolated tests.

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,6 +1,9 @@
+from django.contrib.auth import get_user_model
+User = get_user_model()
 from django.test import TestCase
 from django.core.exceptions import ValidationError
 from lists.models import Item, List
+
 
 class ListAndItemModelsTest(TestCase):
 
@@ -35,7 +38,7 @@ class ListAndItemModelsTest(TestCase):
 
         Item.objects.create(list=list1, text='bla')
         item = Item(list=list2, text='bla')
-        item.full_clean() # should not raise
+        item.full_clean()  # should not raise
 
     def test_list_ordering(self):
         list1 = List.objects.create()
@@ -48,11 +51,42 @@ class ListAndItemModelsTest(TestCase):
         )
 
     def test_string_representation(self):
-        item = Item(text = 'some text')
+        item = Item(text='some text')
         self.assertEqual(str(item), 'some text')
+
 
 class ListModelTest(TestCase):
 
     def test_get_absolute_url(self):
         list_ = List.objects.create()
         self.assertEqual(list_.get_absolute_url(), '/lists/%d/' % (list_.id,))
+
+    def test_create_new_creates_list_and_first_item(self):
+        List.create_new(first_item_text='new item text')
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'new item text')
+        new_list = List.objects.first()
+        self.assertEqual(new_item.list, new_list)
+
+    def test_create_new_optionally_saves_owner(self):
+        user = User.objects.create()
+        List.create_new(first_item_text='new item text', owner=user)
+        new_list = List.objects.first()
+        self.assertEqual(new_list.owner, user)
+
+    def test_lists_can_have_owners(self):
+        List(owner=User())
+
+    def test_list_owner_is_optional(self):
+        List().full_clean()
+
+    def test_create_returns_new_list_object(self):
+        returned = List.create_new(first_item_text='new item text')
+        new_list = List.objects.first()
+        self.assertEqual(returned, new_list)
+
+    def test_list_name_is_first_item_text(self):
+        list_ = List.objects.create()
+        Item.objects.create(list=list_, text='first item')
+        Item.objects.create(list=list_, text='second item')
+        self.assertEqual(list_.name, 'first item')
--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 User = get_user_model()
-from lists.forms import ExistingListItemForm, ItemForm
-from lists.models import Item, List
+from lists.forms import ExistingListItemForm, ItemForm, NewListForm
+from lists.models import List
 
 
 # Create your views here.
@@ -22,13 +22,12 @@ def view_list(request, list_id):
 
 
 def new_list(request):
-    form = ItemForm(data=request.POST)
+    form = NewListForm(data=request.POST)
     if form.is_valid():
-        list_ = List.objects.create()
-        form.save(for_list=list_)
+        list_ = form.save(owner=request.user)
         return redirect(list_)
-    else:
-        return render(request, 'home.html', {"form": form})
+    return render(request, 'home.html', {'form': form})
+
 
 def my_lists(request, email):
     owner = User.objects.get(email=email)
